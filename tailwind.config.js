059753cf520debb2906686ba66157b33c019f2bb
sagar jaid <sagar@sagarjaid.local>
2023-02-14 17:15:47 +0530

    homepage v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      s: "0px",
+      // => @media (min-width: 375px) { ... }
+
+      xs: "375px",
+      // => @media (min-width: 375px) { ... }
+
+      sd: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      sdm: "575px",
+      // => @media (min-width: 575px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      l: "814px",
+      // => @media (min-width: 1024px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      xxl: "1536px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
