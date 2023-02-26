423ae39d0799fd9b6fab4b95c0a99ba65de010a4
sagar jaid <sagar@sagarjaid.local>
2023-02-26 06:27:19 +0530

    GA added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-K8PNJ1RRTG"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-K8PNJ1RRTG');
+// </script>
