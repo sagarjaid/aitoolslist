a7a96d272b2e764ad0d6530b2eadf6601da5dc91
sagar jaid <sagar@sagarjaid.local>
2023-02-27 10:37:52 +0530

    hotjar added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,27 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("hjar");
+        }}
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3384730,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+        }}
+      />
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <Analytics />
